b8f4785b19ceb7eb7971a55f83a672b849eb7703
Jeff Reiner <theundeadghost@gmail.com>
2020-05-08 11:42:11 +0200

    Some content

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "98.css";
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
--- pages/index.js
+++ pages/index.js
@@ -1,198 +1,91 @@
-import Head from 'next/head'
+import Head from "next/head";
+
+const Intro = () => (
+  <section className="window">
+    <header className="title-bar">
+      <div className="title-bar-text">Desktop Photography</div>
+      <div className="title-bar-controls">
+        <button aria-label="Close"></button>
+      </div>
+    </header>
+    <div className="window-body">
+      <blockquote>
+        <p>Screenshots: Desktop Photography</p>
+        <cite>
+          Bhoka [
+          <a
+            href="https://twitter.com/boop"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            @boop
+          </a>
+          ]
+        </cite>
+      </blockquote>
+    </div>
+  </section>
+);
+
+const AuthorLink = ({ author }) => (
+  <a
+    href={`https://twitter.com/${author}`}
+    rel="noopener noreferrer"
+    target="_blank"
+  >
+    <button aria-label="Help"></button>
+    <style jsx>{`
+      a {
+        display: block;
+      }
+      button {
+        cursor: pointer;
+      }
+    `}</style>
+  </a>
+);
+
+const Screenshot = ({ title, author, url }) => (
+  <section className="window">
+    <header className="title-bar">
+      <div className="title-bar-text">{`${title} — ${author}`}</div>
+      <div className="title-bar-controls">
+        <AuthorLink author={author} />
+      </div>
+    </header>
+    <div className="window-body">
+      <img className="window-image" loading="lazy" src={url} alt={title} />
+    </div>
+  </section>
+);
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>desktop.photography</title>
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Intro />
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Screenshot
+          url="/Screen Shot 2020-05-04 at 10.34.27 AM.png"
+          title="Screen Shot 2020-05-04 at 10.34.27 AM"
+          author="@mirshko"
+        />
       </main>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
       <style jsx global>{`
+        *,
+        *::before,
+        *::after {
+          box-sizing: border-box;
+        }
+
         html,
         body {
+          background-color: teal;
           padding: 0;
           margin: 0;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
@@ -200,10 +93,27 @@ export default function Home() {
             sans-serif;
         }
 
-        * {
-          box-sizing: border-box;
+        .window {
+          max-width: 420px;
+          margin: 8px auto 0 auto;
+        }
+
+        .window-image {
+          max-width: 100%;
+          vertical-align: middle;
+          -webkit-user-drag: none;
+        }
+
+        .title-bar-text {
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+
+        blockquote {
+          margin: 0;
         }
       `}</style>
     </div>
-  )
+  );
 }
